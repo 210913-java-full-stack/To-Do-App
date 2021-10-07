39b1d0160ae97a7faee2c1a1a16786bbbcd6ccb3
James Brown <42588084+brownje3@users.noreply.github.com>
2021-10-07 16:44:19 -0400

    "minor changes to xml and added jackson"

---
+++ src/main/java/servlets/PingServlet.java
@@ -0,0 +1,24 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class PingServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException{
+        //Whatever initialization logic I need for this servlet
+    }
+    /*
+    This will take a simple GET request and respond with "Pong!" and status 202, indicating the request was accepted.
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(202);
+        resp.getWriter().print("Pong!");
+    }
+}
+
