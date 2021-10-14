ebe8fa6fb356deb8de05d4e7dce65809aaabfb93
James Brown <42588084+brownje3@users.noreply.github.com>
2021-10-14 11:04:08 -0400

    fixed files

--- src/main/java/servlets/DependencyLoaderListener.java
+++ src/main/java/servlets/DependencyLoaderListener.java
@@ -1,14 +1,5 @@
 package servlets;
 
-<<<<<<< HEAD
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-
-public class DependencyLoaderListener implements ServletContextListener{
-    @Override
-    public void contextInitialized(ServletContextEvent sce) {
-
-=======
 import services.ToDoItemService;
 import utils.ConnectionManager;
 
@@ -21,7 +12,6 @@ public class DependencyLoaderListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         Connection conn = ConnectionManager.getConnection();
         ToDoItemService.init();
->>>>>>> b762675f4359691834b9eb352ea6941b195b4a5d
     }
 
     @Override
