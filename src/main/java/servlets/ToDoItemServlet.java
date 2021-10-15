a1d456afd17b78f4cde4c4d508466f3499ea8406
andrewdpetersen <andrew.petersen@revature.net>
2021-10-15 12:13:47 -0500

    working

--- src/main/java/servlets/ToDoItemServlet.java
+++ src/main/java/servlets/ToDoItemServlet.java
@@ -2,6 +2,7 @@ package servlets;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import models.ToDoItem;
+import repos.ToDoItemRepo;
 import services.ToDoItemService;
 
 import javax.servlet.ServletException;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,8 +32,16 @@ public class ToDoItemServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //super.doPost(req, resp);
+        ToDoItem newItem = new ToDoItem();
+        newItem.setId(-1);
+        newItem.setMessage(req.getParameter("message"));
+        newItem.setComplete(Boolean.parseBoolean(req.getParameter("complete")));
 
-        //TODO: Add in logic to accept new ToDoItems and call some service that will persist them.
+        try {
+            ToDoItemRepo itemRepo = new ToDoItemRepo();
+            itemRepo.save(newItem);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
