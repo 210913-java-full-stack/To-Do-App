194f7c973b94190e20220dcd0c1b0278dd43cbdb
andrewdpetersen <andrew.petersen@revature.net>
2021-10-15 13:59:04 -0500

    adding hibernate wip

--- src/main/java/models/ToDoItem.java
+++ src/main/java/models/ToDoItem.java
@@ -1,26 +1,24 @@
 package models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="to_do_item")
 public class ToDoItem {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column
     private String message;
+
+    @Column
     private boolean complete;
+
 
     public ToDoItem() {
         complete = false;
-    }
-
-    public ToDoItem(String message) {
-        complete = false;
-        this.id = id;
-        this.message = message;
-
-    }
-
-    public ToDoItem(int id, String message) {
-        complete = false;
-        this.id = id;
-        this.message = message;
-
     }
 
     public ToDoItem(int id, String message, boolean complete) {
@@ -53,36 +51,4 @@ public class ToDoItem {
     public void setComplete(boolean complete) {
         this.complete = complete;
     }
-
-    /*
-    This is a list of the 8 java primitive types and the wrapper classes that correspond to them. ~-2,140,000 - ~2,140,000
-     */
-//    public int i; //whole number value, 32-bits signed
-//    public Integer integer;
-//
-//    private float f = 1.2f; //floating point type, decimal value
-//    public Float fl;
-//
-//    public double d = 1.5; //increased precision and length over float
-//    public Double dou;
-//
-//    public long l; //bigger than int
-//    public Long lo;
-//
-//    public boolean b; //true or false
-//    public Boolean bool;
-//
-//    public short s; //smaller than int - 16 bits? signed
-//    public Short sh;
-//
-//    private char c;
-//    public Character ch;
-//
-//    public byte by;
-//    public Byte byt;
-
-
-
-
-
 }
--- src/main/java/servlets/DependencyLoaderListener.java
+++ src/main/java/servlets/DependencyLoaderListener.java
@@ -1,21 +1,34 @@
 package servlets;
 
+import models.ToDoItem;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import services.ToDoItemService;
 import utils.ConnectionManager;
 
+import javax.security.auth.login.AppConfigurationEntry;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import java.sql.Connection;
 
 public class DependencyLoaderListener implements ServletContextListener {
+    SessionFactory sessionFactory;
+    Session session;
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         Connection conn = ConnectionManager.getConnection();
         ToDoItemService.init();
+
+        Configuration config = new Configuration().configure("hibernate.properties");
+        config.addAnnotatedClass(ToDoItem.class);
+        sessionFactory = config.buildSessionFactory();
+        session = sessionFactory.openSession();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-
+        session.close();
     }
 }
