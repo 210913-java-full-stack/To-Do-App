2cd468a1eb60f8a4e8904dd5deb42fabdfd79958
Hawkx1 <gregg.friedman@revature.net>
2021-10-18 11:48:54 -0400

    Added FileLogger

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,4 +1,5 @@
 
+import utils.FileLogger;
 import utils.ViewManager;
 
 
@@ -8,26 +9,7 @@ import java.sql.*;
 //This is where our program starts, with the main method and contains the menus and primary loop.
 public class Driver {
     public static void main(String[] args) {
-        //viewManager
-        ViewManager viewManager = ViewManager.getViewManager();
-
-        //connection
-        //Connection conn = ConnectionManager.getConnection();
-
-
-        /**
-         * This is our main loop, it keep running until something sets the viewManager "running" flag = false;
-         * With every loop we invoke the viewManager singleton's goToNextView() method.
-         */
-        viewManager.navigate("MainMenu");
-        while(viewManager.isRunning()) {
-            try{
-                viewManager.goToNextView();
-            } catch(SQLException e) {
-                e.printStackTrace();
-            }
-
-        }
+        FileLogger.getFileLogger().writeLog("Test message", 0);
     }
 }
 
