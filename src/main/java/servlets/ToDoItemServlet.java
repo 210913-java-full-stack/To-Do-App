954e77a442fc6821056429bdc8545ffcff1ccac3
Hawkx1 <gregg.friedman@revature.net>
2021-10-24 14:58:01 -0400

    Copied FileLogger to this branch

--- src/main/java/servlets/ToDoItemServlet.java
+++ src/main/java/servlets/ToDoItemServlet.java
@@ -8,31 +8,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
-<<<<<<< HEAD
-import java.util.stream.Collectors;
-=======
 import java.util.Scanner;
->>>>>>> 31e7d02c7d0ecf7d68b6c65cc8ee2a1562c8ec66
 
 public class ToDoItemServlet extends HttpServlet {
 
 
     @Override
-<<<<<<< HEAD
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
-
-        System.out.println("ToDoItem Servlet GET request...");
-
-=======
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
->>>>>>> 31e7d02c7d0ecf7d68b6c65cc8ee2a1562c8ec66
         List<ToDoItem> toDoItemList = ToDoItemService.getAllToDoItems();
         ObjectMapper mapper = new ObjectMapper();
         resp.getWriter().write(mapper.writeValueAsString(toDoItemList));
@@ -41,17 +27,6 @@ public class ToDoItemServlet extends HttpServlet {
     }
 
     @Override
-<<<<<<< HEAD
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        ToDoItem newItem = new ToDoItem();
-        ObjectMapper mapper = new ObjectMapper();
-        System.out.println("JSON received");
-        String requestData = req.getReader().lines().collect(Collectors.joining());
-        newItem = mapper.readValue(requestData,ToDoItem.class);
-        System.out.println(requestData);
-        System.out.println("Item received");
-        ToDoItemService.saveNewToDoItem(newItem);
-=======
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         /*
         if we need to invoke different logic depending on what's sent to this servlet
@@ -72,8 +47,6 @@ public class ToDoItemServlet extends HttpServlet {
             case "something-else":
                 break;
         }
-
->>>>>>> 31e7d02c7d0ecf7d68b6c65cc8ee2a1562c8ec66
     }
 
 
